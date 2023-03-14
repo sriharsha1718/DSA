34545f0fd3953872d9e18e64a1528fd92b34b3fe
Sri Harsha Thatha <91465207+sriharsha1718@users.noreply.github.com>
2023-03-14 11:41:53 +0530

    Add files via upload

---
+++ Graphs/Graphs_L1.py
@@ -0,0 +1,43 @@
+#Adjacency Matrix and Adjacency Lists
+
+############################################
+#Representation of Graph in Adjacency Matrix
+ipt = [[0,1],[0,2],[0,3],[0,4],[1,3],[2,3],[2,4],[2,5],[3,5]]
+#N must be 6 for this input
+N = int(input("Enter no of nodes in the graph:"))
+adj_matrix = []
+for i in range(N):
+    temp_list = []
+    for j in range(N):
+        temp_list.append(0)
+    adj_matrix.append(temp_list)
+print("The initialised adjacency matrix is:")
+for item in adj_matrix:
+    print(item)
+
+#Altering the initialised matrix with given input
+for (u,v) in ipt:
+    adj_matrix[u][v] = 1
+    adj_matrix[v][u] = 1
+    
+print("\nThe Final adjacency matrix is:")
+for item in adj_matrix:
+    print(item) 
+
+##########################################
+#Representation of Graph in Adjacency List
+adj_list = {}
+for i in range(N):
+    adj_list[i] = []
+
+print("\nThe initialised adjacency list is:")
+for item in adj_list.items():
+    print(item)
+
+for (u,v) in ipt:
+    adj_list[u].append(v)
+    adj_list[v].append(u)
+
+print("\nThe final adjacency list is:")
+for item in adj_list.items():
+    print(item)
